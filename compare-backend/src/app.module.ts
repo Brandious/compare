1405fd8cf79bc6d23777360891a3a050ea052299
afacic <afacic@collectivemind.dev>
2024-05-27 03:32:27 +0200

    FirstCommit

---
+++ compare-backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ limit: 10, ttl: 60 }]),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService], // Add this line
+})
+export class AppModule {}
---
+++ compare-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Box>
+        <h1>React App</h1>
+      </Box>
+    </>
+  );
+}
+
+export default App;
